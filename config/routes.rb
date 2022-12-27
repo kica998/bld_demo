57747eaa014fafa6093147d1b4984cd02a4a0c63
Al Gold <algold@als-macbook-pro.lan>
2022-12-26 19:46:25 -0800

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :posts
   root 'pages#home'
   get 'about', to: 'pages#about'
  # get 'home', to: 'pages#home'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# @post = Post.create(title: "crocodile", body: "See you later alligator")
+ 
